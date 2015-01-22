84223e50dd0762502b29396655ae505939721e17
Priit Lätt <priit.latt@lab.mobi>
2015-01-22 15:31:01 +0200

    Add appium tests

--- GreenhouseCI/appium/java/src/test/java/com/greenhouseci/FailureTest.java
+++ GreenhouseCI/appium/java/src/test/java/com/greenhouseci/FailureTest.java
@@ -3,11 +3,12 @@ package com.greenhouseci;
 import com.greenhouseci.util.AppiumTest;
 import static com.greenhouseci.util.Helpers.*;
 import org.openqa.selenium.WebElement;
+import org.junit.Test;
 
-public class MyTest extends AppiumTest {
+public class FailureTest extends AppiumTest {
 
-    @org.junit.Test
-    public void hasText() throws Exception {
-        text("Thank you");
+    @Test
+    public void doesNotHaveText() throws Exception {
+        text("no such text");
     }
 }
--- GreenhouseCI/appium/java/src/test/java/com/greenhouseci/SuccessTest.java
+++ GreenhouseCI/appium/java/src/test/java/com/greenhouseci/SuccessTest.java
@@ -3,11 +3,12 @@ package com.greenhouseci;
 import com.greenhouseci.util.AppiumTest;
 import static com.greenhouseci.util.Helpers.*;
 import org.openqa.selenium.WebElement;
+import org.junit.Test;
 
-public class MyTest extends AppiumTest {
+public class SuccessTest extends AppiumTest {
 
-    @org.junit.Test
-    public void hasText() throws Exception {
-        text("Thank you");
+    @Test
+    public void hasText2() throws Exception {
+        text("GreenhouseCI");
     }
 }
