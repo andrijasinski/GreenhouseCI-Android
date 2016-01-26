a64007aea7c430b5e0a3d71fab210c11bfa63888
GreenhouseCI Build System <team@greenhouseci.com>
2016-01-26 12:50:55 +0200

    Add crashlytics

--- GreenhouseCI/src/main/java/com/greenhouseci/MainActivity.java
+++ GreenhouseCI/src/main/java/com/greenhouseci/MainActivity.java
@@ -2,12 +2,15 @@ package com.greenhouseci;
 
 import android.os.Bundle;
 import android.app.Activity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
     
